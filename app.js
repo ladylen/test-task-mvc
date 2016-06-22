c03a1eb12c394abc50fc8c5fac10f7e8afbac28f
ladylen <ladylen@gmail.com>
2016-06-22 22:07:05 +0300

    Add files via upload
    - добавлены на страницу JSON наборы City, Country
    - добавлена форма для добавления города (добавляет на страницу, но в JSON не сохраняет; нет проверки на пустые поля - не знаю пока как это сделать; )
    - выведены на страницу города (поля для описания сразу edit-ами; сделан "remove" со страницы)
    
    Проблемы:
    - надо связать страны и города, чтоб выводить города страны
    - сохранять изменения в JSON (добавленные новые города и их описания), либо привязывать БД?
    - глобальное недопонимания бэкграунда - что и как происходит внутри (буду ещё искать видео курсы :)

---
+++ app.js
@@ -0,0 +1,31 @@
+
+(function() {
+  var app = angular.module('world', [ ]);
+
+  app.controller("WorldController", function($scope, $http) {
+      $http.get("countries.json").success(function(response){
+          $scope.countries = response;
+      });
+      $http.get("cities.json").success(function(response){
+          $scope.cities = response;
+      });
+
+      this.addCity = function(city) {
+	      //if(city) {
+              $scope.cities.push(this.city);
+              this.city = {};
+	      //}
+      };
+      this.remCity = function(city) {
+          var array = $scope.cities;
+          for (var i = 0; i < array.length; i++) {
+              if (array[i] === city) {
+                  array.splice(i,1);
+                  return 0;
+              }
+          }
+      };
+  
+    });
+
+})();

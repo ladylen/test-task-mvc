361574d9137c3cf669ff1682eb7255d7c73dcc40
alex-arkhypenko <alex.arkhypenko@gmail.com>
2016-06-29 21:50:02 +0300

    ngResource first steps to use

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 
 (function() {
-  var app = angular.module('world', [ ]);
-
+  var app = angular.module('myApp', [ 'ngResource' ]);
+  
+  app.run(
+            function( $resource ) {
+	      var countries =  $resource(
+		"http://localhost:3000/countries",
+		
+	    });
+  
+  
+  
   app.controller("contriesCtrl", function($scope, $http) {
       $http.get("countries.json").success(function(response){
           $scope.countries = response;

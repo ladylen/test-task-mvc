be9bcfa7c3474b023f230fe4f31c2aacc50ef41b
alex-arkhypenko <alex.arkhypenko@gmail.com>
2016-07-14 13:15:45 +0300

    Complited adding the city. Started city editing.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 (function() {
   var app = angular.module('myApp', [ 'ngResource' ]);
-  
+
 //   app.factory('Country', function( $resource ) {
 // 	      return $resource("http://localhost:3000/countries");
-// 		
+//
 // 	    });
-    
+
   app.controller("CountriesController", function() {
-	
+
 	  this.countries = cou;
 	  this.cities = ci;
 	  this.cityTableVis = false;
@@ -31,8 +31,8 @@
 	  }
 
 	 // Delete the City
-	 this.remCity = function(city) {
-           var array = this.cities;
+  this.remCity = function(city) {
+        var array = this.cities;
 	    array.forEach(function(item,i,ci){
 
  	    if(item === city) {
@@ -40,27 +40,43 @@
  	    }
 	  });
 	    this.cities = array; // na vsiakiy sluchay
-	 }	  
+	 }
+
+     this.addCity = function(ci, couId) {
+         //alert(ci.title+ci.desc+couId);
+         var city = {};
+         city.id = this.cities.length+1;
+         city.country_id = couId;
+         city.title = ci.title;
+         city.desc = ci.desc;
+         this.cities.push(city);
+         this.showCities(couId);
+
+         //alert(city.id);
+     }
+
+
   });
-  
+
+
   var cou = [
 {
-"id": 1, 
+"id": 1,
 "title":"United Kingdom",
 "text":"The United Kingdom of Great Britain and Northern Ireland, commonly known as the United Kingdom (UK) or Britain, is a sovereign state in Europe."
 },
 {
-"id": 2, 
+"id": 2,
 "title":"France",
 "text":"France, officially the French Republic (French: R\u00e9publique fran\u00e7aise), is a unitary sovereign state comprising territory in western Europe and several overseas regions and territories."
 },
 {
-"id": 3, 
+"id": 3,
 "title":"Spain",
 "text":"Spain (Spanish: Espa\u00f1a), officially the Kingdom of Spain (Spanish: Reino de Espa\u00f1a), is a sovereign state located on the Iberian Peninsula in southwestern Europe."
 },
 {
-"id": 4, 
+"id": 4,
 "title":"Germany",
 "text":"Germany, officially the Federal Republic of Germany (German: Bundesrepublik Deutschland), is a federal parliamentary republic in western-central Europe."
 }
@@ -68,51 +84,51 @@
 
   var ci = [
 {
-"id": 1, 
-"country_id": 1, 
-"title": "London", 
+"id": 1,
+"country_id": 1,
+"title": "London",
 "desc": "",
 "vis": false
 },
 {
-"id": 2, 
-"country_id": 1, 
-"title": "Liverpool", 
+"id": 2,
+"country_id": 1,
+"title": "Liverpool",
 "desc": "",
 "vis": false
 },
 {
-"id": 3, 
-"country_id": 2, 
-"title": "Paris", 
+"id": 3,
+"country_id": 2,
+"title": "Paris",
 "desc": "Go to Paris",
 "vis": false
 },
 {
-"id": 4, 
-"country_id": 3, 
-"title": "Madrid", 
+"id": 4,
+"country_id": 3,
+"title": "Madrid",
 "desc": "",
 "vis": false
 },
 {
-"id": 5, 
-"country_id": 4, 
-"title": "Berlin", 
+"id": 5,
+"country_id": 4,
+"title": "Berlin",
 "desc": "",
 "vis": false
 },
 {
-"id": 6, 
-"country_id": 4, 
-"title": "Munich", 
+"id": 6,
+"country_id": 4,
+"title": "Munich",
 "desc": "",
 "vis": false
 },
 {
-"id": 7, 
-"country_id": 4, 
-"title": "Hamburg", 
+"id": 7,
+"country_id": 4,
+"title": "Hamburg",
 "desc": "",
 "vis": false
 }
